1ca937179ecc72203d2f4c8bda2f7e89c8d40401
codnrawbwj <codnrawbwj@gmail.com>
2024-11-14 20:23:33 -0600

    Move assets directory and remove unused code

--- js/darkmode.js
+++
@@ -1,6 +0,0 @@
-let darkmode = localStorage.getItem('darkmode');
-const darkModeToggle = document.querySelector("#dark-mode-toggle");
-
-darkModeToggle.addEventListener('click', () => {
-    console.log('Daaaaark Mooooode!');
-})
