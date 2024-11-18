2aad1656775563676002d6728d9c81f026b7651a
codnrawbwj <codnrawbwj@gmail.com>
2024-11-17 23:36:39 -0600

    Create HomeSection comp

---
+++ timothylim/src/components/HomeSection.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeSection = () => {
+  return (
+    <section className="wrapper min-h-[90vh] flex items-center">
+      <div className="text-[3rem] leading-[1.6] tracking-wide ml-4">
+        <h1 className="underline text-underline-offset-0 decoration-[6px] decoration-secondary">
+          CHAE-UK LIM <span className="text-[2rem]">aka</span> TIMOTHY
+        </h1>
+        <p>FULL-STACK</p>
+        <p>SOFTWARE ENGINEER</p>
+      </div>
+    </section>
+  );
+};
+
+export default HomeSection;
