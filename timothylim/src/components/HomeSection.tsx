46e49a6c658496f8b19b8e428402aa8a4fb73bf0
codnrawbwj <codnrawbwj@gmail.com>
2024-11-18 19:12:59 -0600

    Add FramerMotion animation on HomeSection comp

--- timothylim/src/components/HomeSection.tsx
+++ timothylim/src/components/HomeSection.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+"use client";
+import { motion } from "motion/react";
 
 const HomeSection = () => {
   return (
-    <section className="wrapper min-h-[90vh] flex items-center">
-      <div className="text-[3rem] leading-[1.6] tracking-wide ml-4">
-        <h1 className="underline text-underline-offset-0 decoration-[6px] decoration-secondary">
-          CHAE-UK LIM <span className="text-[2rem]">aka</span> TIMOTHY
-        </h1>
-        <p>FULL-STACK</p>
-        <p>SOFTWARE ENGINEER</p>
-      </div>
-    </section>
+    <motion.div
+      initial={{ x: -400 }}
+      animate={{ x: 0 }}
+      transition={{ ease: "easeIn", duration: 0.7 }}
+    >
+      <section className="wrapper min-h-[90vh] flex items-center font-primary">
+        <div className="text-[3rem] leading-[1.6] tracking-wide ml-4">
+          <h1 className="underline text-underline-offset-0 decoration-[6px] decoration-secondary">
+            CHAE-UK LIM <span className="text-[2rem]">aka</span> TIMOTHY
+          </h1>
+          <p>FRONT-END</p>
+          <p>SOFTWARE ENGINEER</p>
+        </div>
+      </section>
+    </motion.div>
   );
 };
 
