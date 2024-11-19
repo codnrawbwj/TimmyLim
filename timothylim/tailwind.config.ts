572ad69f0b8e99dd1be8908e0f0adf4d4305a3f4
codnrawbwj <codnrawbwj@gmail.com>
2024-11-18 20:21:03 -0600

    Add additional primary colors

--- timothylim/tailwind.config.ts
+++ timothylim/tailwind.config.ts
@@ -16,6 +16,8 @@ const config: Config = {
     extend: {
       colors: {
         primary: "#F6FCDF",
+        "primary-b": "#c2eabd",
+        "primary-c": "#35ce8d",
         secondary: "#859F3D",
         accent: "#31511E",
         dark: "#1A1A19",
