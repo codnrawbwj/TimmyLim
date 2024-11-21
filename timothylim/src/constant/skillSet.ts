9582d0839dee807b33c949a552b4a2e0cd28f9d6
codnrawbwj <codnrawbwj@gmail.com>
2024-11-20 23:32:27 -0600

    Create constant skillset file

---
+++ timothylim/src/constant/skillSet.ts
@@ -0,0 +1,6 @@
+export const skillSet = {
+  Language: ["javascript", "typescript", "python"],
+  FrontEnd: ["html", "css", "react", "nextjs", "tailwind", "sass"],
+  BackEnd: ["nodejs", "mongoDB", "graphQL"],
+  ETC: ["git", "figma", "jest"],
+};
