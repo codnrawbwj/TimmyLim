393ba9998f870b437c042effbfc78eda264ac06b
codnrawbwj <codnrawbwj@gmail.com>
2024-11-21 22:30:17 -0600

    Create Project section

--- timothylim/src/app/page.tsx
+++ timothylim/src/app/page.tsx
@@ -1,6 +1,7 @@
 import AboutMe from "@/components/AboutMe";
 import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <HomeSection />
       <AboutMe />
       <Skills />
+      <Projects />
     </>
   );
 }
---
+++ timothylim/src/components/Projects.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <section className="wrapper min-h-[100vh]"></section>;
+};
+
+export default Projects;
