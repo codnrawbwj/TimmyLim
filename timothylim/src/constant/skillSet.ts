790a3189574f285a557ea588d74b054ab6adc471
codnrawbwj <codnrawbwj@gmail.com>
2024-11-21 22:15:21 -0600

    Remove unnecessary code

--- timothylim/src/constant/skillSet.ts
+++
@@ -1,6 +0,0 @@
-export const skillSet = {
-  Language: ["javascript", "typescript", "python"],
-  FrontEnd: ["html", "css", "react", "nextjs", "tailwind", "sass"],
-  BackEnd: ["nodejs", "mongoDB", "graphQL"],
-  ETC: ["git", "figma", "jest"],
-};
