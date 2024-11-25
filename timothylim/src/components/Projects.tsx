dafb04e47a34c8392fddd403123280bf58975a44
codnrawbwj <codnrawbwj@gmail.com>
2024-11-24 22:12:40 -0600

    Remove unused images

--- timothylim/src/components/Projects.tsx
+++ timothylim/src/components/Projects.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Projects = () => {
-  return <section className="wrapper min-h-[100vh]"></section>;
+  return (
+    <section className="wrapper min-h-[100vh]">
+      <div>
+        <h2 className="text-[2rem] underline font-medium underline-offset-8 decoration-primary-b">
+          Projects
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
